65b3195cfe16dd9462756dffc19f7be3bd892853
Suhanov Ivan <ivan.suhanov5@yandex.ru>
2023-07-02 18:08:02 +0300

    added selection sort

--- src/DifferentAlgorithms.java
+++ src/DifferentAlgorithms.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DifferentAlgorithms {
     public int binarySearch(int[] list, int item){
         int low = 0;
@@ -17,5 +20,28 @@ public class DifferentAlgorithms {
             }
         }
         return -1;
+    }
+
+    public List<Integer> selectionSort(List<Integer> arr){
+        int size = arr.size();
+        List<Integer> sortedArr = new ArrayList<>(size);
+
+        for (int i = 0; i < size; i++) {
+            sortedArr.add(arr.remove(findSmallest(arr)));
+        }
+        return sortedArr;
+    }
+
+    public int findSmallest(List<Integer> arr){
+        int smallest = arr.get(0);
+        int smallestIndex = 0;
+        for (int i = 1; i < arr.size(); i++) {
+            if(smallest > arr.get(i)){
+                smallestIndex = i;
+                smallest = arr.get(i);
+            }
+        }
+
+        return smallestIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
@@ -13,6 +14,16 @@ public class Main {
             list[i] = i + 1;
         }
         System.out.println("Ищем число: " + search + " в массиве чисел от 1 до " + arrLength);
-        System.out.println("Искомое число находится на позиции " + differentAlgorithms.binarySearch(list,search));
+        System.out.println("Искомое число имеет индекс " + differentAlgorithms.binarySearch(list,search));
+
+
+        ArrayList<Integer> integerArrayListForSorted = new ArrayList<>(arrLength);
+        for (int i = 0; i < arrLength; i++) {
+            integerArrayListForSorted.add(random.nextInt(arrLength*10));
+        }
+        System.out.println("Неупорядоченный масиив");
+        System.out.println(integerArrayListForSorted);
+        System.out.println("Массив после сортировки");
+        System.out.println(differentAlgorithms.selectionSort(integerArrayListForSorted));
     }
 }
